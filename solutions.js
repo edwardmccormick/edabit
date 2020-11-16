4abfa072d96a212ee6d6f35da638d14e8779f332
edwardmccormick <ted.mccormick@gmail.com>
2020-11-15 21:22:23 -0600

    Adding more solutions

--- solutions.js
+++ solutions.js
@@ -8,3 +8,83 @@ function convert(minutes) {
 function addition(num) {
     return ++num
 }
+
+function howManySeconds(hours) {
+    return hours*60*60
+}
+
+function calcAge(age) {
+    return age*365
+}
+
+function findPerimeter(length, width) {
+    return length*2+width*2
+}
+
+function squared(a) {
+    return a * a
+}
+
+function getFirstValue(arr) {
+    return arr[0]
+}
+
+function circuitPower(voltage, current) {
+    return voltage*current
+}
+
+function giveMeSomething(a) {
+    return "something " + a
+}
+
+function lessThanOrEqualToZero(num) {
+    return num <= 0
+}
+
+function animals(chickens, cows, pigs) {
+    return chickens*2 + cows*4 + pigs*4
+}
+
+function cubes(a) {
+    return a ** 3
+}
+
+function triArea(base, height) {
+    return base*height/2
+}
+
+function and(a, b) {
+    return a && b
+}
+
+function profitableGamble(prob, prize, pay) {
+    return prob * prize > pay;
+}
+
+function nextEdge(side1, side2) {
+    return (side1 + side2) - 1
+}
+
+function footballPoints(wins, draws, losses) {
+    return wins*3+draws*1;
+}
+
+function frames(minutes, fps) {
+    return minutes *fps * 60
+}
+
+function checkEquality(a, b) {
+    return a === b
+}
+
+function convert(hours, minutes) {
+    return (hours * 60 + minutes) *60
+}
+
+function lessThan100(a, b) {
+    return a+b<100
+}
+
+function addition(a, b) {
+    return a+b
+}
